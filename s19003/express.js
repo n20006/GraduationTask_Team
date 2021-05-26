b6d1e4bb92afe065967869c170d3f8cb797d8f2c
s19003 <s19003@std.it-college.ac.jp>
2021-05-27 04:02:32 +0900

    express

--- s19003/express.js
+++ s19003/express.js
@@ -1,27 +1,18 @@
 const express = require('express')
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!')
-})
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
+// POST
 app.post('/', (req, res) => {
-  res.send('Got a POST request')
+  res.send(req.body)
 })
 
-app.put('/user', (req, res) => {
-  res.send('Got a request at /user')
-})
-
-app.delete('/user', (req, res) => {
-  res.send('Got a DELETE request at /user')
-})
-
-app.all('/all', (req, res, next) => {
-  console.log('ALL')
-})
-
+// サーバーを起動する処理
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
