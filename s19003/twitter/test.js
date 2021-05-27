26e82b35c3527d151aa9c16021f2a4d839738c31
s19003 <s19003@std-it.college.ac.jp>
2021-05-27 12:38:05 +0900

    sample

--- s19003/twitter/test.js
+++ s19003/twitter/test.js
@@ -1,5 +1,11 @@
 // import Twitter from 'twitter-v2'
 import dotenv from 'dotenv'
+import Twitter from 'twitter-v2'
 dotenv.config()
 
-console.log(process.env.BEARER_TOKEN)
+const client = new Twitter({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_SECRET
+})
